6c4b0c6aaa142aa9d89a41100912141abde48e11
MyeonuK <gusdn0217@gmail.com>
2022-03-02 22:46:15 +0900

    9498 => 해결

---
+++ if문/9498.js
@@ -0,0 +1,9 @@
+// 시험 성적
+
+let score = Number(require("fs").readFileSync("/dev/stdin").toString());
+
+if (score >= 90) console.log("A");
+else if (score >= 80) console.log("B");
+else if (score >= 70) console.log("C");
+else if (score >= 60) console.log("D");
+else console.log("F");
